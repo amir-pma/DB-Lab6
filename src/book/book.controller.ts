7e040408453c5f17a99b60c90b7e362ef457a750
amirpma <amir.pma1378@gmail.com>
2020-12-08 00:34:40 +0330

    part2 - added book controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import {ApiResponse} from '@nestjs/swagger'; 
+import { BooksServices } from './book.service';
+import CreateBookDto from './dto/create-book.dto';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BooksController {
+    constructor(private readonly booksServices: BooksServices) {}
+
+    @Post('post')
+    postBook( @Body() genre: CreateBookDto) {
+        return this.booksServices.insert(genre);
+    }
+
+    @Get()
+    getAll() {
+        return this.booksServices.getAllBooks();
+    }
+}
