547bb835829040140ec4af140d7577fe067d99b0
amirpma <amir.pma1378@gmail.com>
2020-12-08 04:05:34 +0330

    part2 - fixed class validation warning

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export default class CreateUserDto {
-    @ApiProperty({type: "string" ,description:"Enter user's name"}) 
+    @ApiProperty({type: "string" ,description:"Enter user's name"})
     readonly name: string;
 
     @ApiProperty({type: "array", items: {type: "number"} ,description:"Enter ids of the books of the user."}) 
