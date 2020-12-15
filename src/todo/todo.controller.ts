ad13975a87685f318f2e9a883c16195d926f1681
amirpma <amir.pma1378@gmail.com>
2020-12-16 00:24:30 +0330

    added todo controller for categories

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Delete, Put, Query } from '@nestjs/common';
+import { ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { TodoService } from './todo.service';
 
-@Controller('todo')
-export class TodoController {}
+
+@Controller('books')
+export default class TodoController {
+    constructor(private readonly todoServices: TodoService) {}
+
+    @ApiResponse({ status: 200, description: "Adds new category to database" }) 
+    @ApiBearerAuth()
+    @Post('post')
+    insertCategory( @Body() category: CreateCategoryDto) {
+        return this.todoServices.insertCategory(category);
+    }
+
+    @ApiResponse({ status: 200, description: "Gets all the books in database" }) 
+    @ApiBearerAuth()
+    @Get()
+    getAll() {
+        return this.booksServices.getAllBooks();
+    }
+
+    @ApiResponse({ status: 200, description: "Deletes a book from database" })
+    @ApiBearerAuth()
+    @ApiQuery({
+        name: 'bookID',
+        required: true,
+        type: Number,
+        description :`id of book being deleted`
+    })
+    @Delete('delete')
+    deleteBook(@Query('bookID') bookID) {
+        return this.booksServices.delete(bookID);
+    }
+
+    @ApiResponse({ status: 200, description: "Updates an existing book in database" })
+    @ApiBearerAuth()
+    @Put('update')
+    updateBook(@Body() book: UpdateBookDto) {
+        return this.booksServices.update(book);
+    }
+}
--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Module({
-  providers: [TodoService],
+  providers: [TodoService, {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  }],
   controllers: [TodoController]
 })
 export class TodoModule {}
