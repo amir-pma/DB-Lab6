59ed25f15bd163e803ad40590e23f0141c39c0bc
amirpma <amir.pma1378@gmail.com>
2020-12-16 03:40:45 +0330

    final project cleaning

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
-import { TodoController } from './todo.controller';
+import TodoController  from './todo.controller';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
